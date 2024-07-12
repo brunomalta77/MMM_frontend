6a96e833560387521f4907547c0d6f6843b04fc3
brunomalta77 <83467995+brunomalta77@users.noreply.github.com>
2024-07-12 11:11:02 +0100

    Update scenario_optimisation.py

--- scenario_optimisation.py
+++ scenario_optimisation.py
@@ -975,7 +975,7 @@ def main():
                           
               
                 with logout_container:
-                       col1, col2,col3, = st.columns([10, 1,1])
+                       col1, col2,col3, = st.columns([5, 1,1])
                        with col2:
                                 if st.session_state.get('access', False):
                                          if st.button("Logout", key="logout_button"):
