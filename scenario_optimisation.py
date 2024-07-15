0bae63daa373c79ded61d90ba8259081c1cbc550
brunomalta77 <83467995+brunomalta77@users.noreply.github.com>
2024-07-15 10:30:24 +0100

    Update scenario_optimisation.py

--- scenario_optimisation.py
+++ scenario_optimisation.py
@@ -983,7 +983,7 @@ def main():
                 """
                  
                 with logout_container:
-                       col1, col2,col3 = st.columns([9,1,1])
+                       col1, col2,col3 = st.columns([8,1,1])
                        with col2:
                                 components.html(html_code, height=3)
                                 st.markdown(f'<p style="font-size:12px;">{st.session_state.user_email}</p>', unsafe_allow_html=True)
