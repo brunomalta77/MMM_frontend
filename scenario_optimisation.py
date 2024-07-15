8a2f4cbf6c1709bc43dc26cb89d72179c46b4e94
brunomalta77 <83467995+brunomalta77@users.noreply.github.com>
2024-07-15 11:12:18 +0100

    Update scenario_optimisation.py

--- scenario_optimisation.py
+++ scenario_optimisation.py
@@ -969,33 +969,33 @@ def main():
                     
             # Custom CSS to push the logout button to the right and style it
             # Custom CSS for the smaller logout button and layout
-             st.markdown("""
-                            <style>
-                            #root > div:nth-child(1) > div > div > div > div > section > div {padding-top: 2rem;}
-                            .logout-button {
-                                position: fixed;
-                                top: 0.2rem;
-                                right: 0.5rem;
-                                z-index: 999999;
-                            }
-                            .logout-button button {
-                                background-color: #ff4b4b;
-                                color: red;
-                                border: none;
-                                padding: 0.1rem 0.3rem;
-                                text-align: center;
-                                text-decoration: none;
-                                display: inline-block;
-                                font-size: 0.1rem;
-                                margin: 3px;
-                                cursor: pointer;
-                                border-radius: 2px;
-                            }
-                            .logout-button button:hover {
-                                background-color: #ff0000;
-                            }
-                            </style>
-                            """, unsafe_allow_html=True)  
+            st.markdown("""
+                    <style>
+                    #root > div:nth-child(1) > div > div > div > div > section > div {padding-top: 2rem;}
+                    .logout-button {
+                        position: fixed;
+                        top: 0.2rem;
+                        right: 0.5rem;
+                        z-index: 999999;
+                    }
+                    .logout-button button {
+                        background-color: #ff4b4b;
+                        color: red;
+                        border: none;
+                        padding: 0.1rem 0.3rem;
+                        text-align: center;
+                        text-decoration: none;
+                        display: inline-block;
+                        font-size: 0.1rem;
+                        margin: 3px;
+                        cursor: pointer;
+                        border-radius: 2px;
+                    }
+                    .logout-button button:hover {
+                        background-color: #ff0000;
+                    }
+                    </style>
+                    """, unsafe_allow_html=True)  
             # Custom HTML and CSS to adjust col2 position
             html_code = """
             <div style="margin-left: 20px;">
